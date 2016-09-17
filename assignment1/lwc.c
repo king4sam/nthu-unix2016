2cf4ae003e774777b866dc09fd0245463892014a
necisam <king4sam990816@gmail.com>
2016-09-18 01:52:20 +0800

    regex parse single options

--- assignment1/lwc.c
+++ assignment1/lwc.c
@@ -9,6 +9,38 @@ int main (int argc, char **argv){
   FILE* fp;
   char ch;
   char* re_pattern= "-([lwc]+)";
+  size_t nmatch = 2;
+  regex_t reg;
+  regmatch_t pmatch[ nmatch ];
+  int flag = REG_EXTENDED|REG_ICASE;
+  if( regcomp( &reg , re_pattern  , flag ) != 0 ){
+    printf( "regexp comp error.\n" );
+    exit(1);
+  }
+  int j = 0;
+  if (regexec( &reg , argv[1] , nmatch , pmatch , 0 ) == 0){
+    int match_len = pmatch[1].rm_eo - pmatch[1].rm_so;
+    printf(" match length %d\n", match_len);
+    char* m = malloc((match_len+1)* sizeof(char) );
+    m[match_len] = '\0';
+    strncpy(m, &argv[1][pmatch[1].rm_so],match_len);
+    printf("%d \n",strlen(m));
+    printf("args is %s\n",m);
+
+    for(j = 0; j < match_len;j++){
+      switch (m[j]){
+        case 'l':
+          printf("it's l\n");
+          break;
+        case 'w':
+          printf("it's w\n");
+          break;
+        case 'c':
+          printf("it's c\n");
+          break;
+      }
+    }
+  }
 
   if(argc < 3){
     printf("There are too few args\n");
