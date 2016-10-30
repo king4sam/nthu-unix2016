3ced5d1eebdad77d76ae343720203acb855a6b9a
necisam <king4sam990816@gmail.com>
2016-10-30 13:03:31 +0800

    without cp hole to tarfile

--- assignment3/workspace/lcp.c
+++ assignment3/workspace/lcp.c
@@ -3,10 +3,9 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <sys/stat.h>
-
+#include <sys/types.h>
 
 #define BUFFSIZE 4096
-
 
 int main(int argc, char **argv){
   char buf[BUFFSIZE];
@@ -19,16 +18,54 @@ int main(int argc, char **argv){
   srcfd < 0 ? fprintf(stderr, "src open failed\n" ): printf("src fd %d\n", srcfd);
 
   struct stat src_stat;
-  fstat( srcfd, &src_stat) == -1 ? fprintf(stderr, "get stat err\n"):printf("get stat suc");
+  fstat( srcfd, &src_stat) == -1 ? fprintf(stderr, "get stat err\n"):printf("get stat suc\n");
+  off_t n_offset = 0;
+  // off_t next_data = lseek(srcfd,n_offset,SEEK_DATA);
+
+  // int dr = read(srcfd, buf, BUFFSIZE);
+  // printf("read %d, data is :%s\n", dr,buf);
+
+  // printf("data at %ld\n", next_data);
+
+
+  // off_t next_hole = lseek(srcfd, 0,SEEK_HOLE);
+
+  // lseek(srcfd,0,SEEK_SET);
+  // for (int i = 0; i < next_hole; ++i)
+  // {
+  //   read(srcfd, buf, 1);
+  //   if(buf[0] == 0){
+  //     putchar('\0');
+  //   }
+  //   else{
+  //     putchar((char)buf[0] );
+  //   }
+  // }
+
+  // printf("hole at %ld\n", next_hole);
+
+  // next_data = lseek(srcfd,next_hole,SEEK_DATA);
+  // printf("data at %ld\n", next_data);
+
   int tarfd = open(tar_filename, O_WRONLY|O_CREAT | O_EXCL,src_stat.st_mode);
+  if(tarfd < 0){
+    tarfd = open(tar_filename, O_WRONLY);
+  }
   tarfd < 0 ? fprintf(stderr, "tar open failed\n" ): printf("tar fd %d\n", tarfd);
 
   int bytes_read;
-  while ((bytes_read = read(srcfd, buf, BUFFSIZE)) > 0){
-    if (write(tarfd, buf, bytes_read) != bytes_read)
-      fprintf(stderr, "write error\n" );
-    if (bytes_read < 0)
-      fprintf(stderr, "read error\n" );
+  while ((bytes_read = read(srcfd, buf, 1)) > 0){
+    if(buf[0] == 0){
+      buf[0] = '\0';
+      write(tarfd,buf,bytes_read);
+    }
+    else{
+      write(tarfd,buf,bytes_read);
+    }
+  //   if (write(tarfd, buf, bytes_read) != bytes_read)
+  //     fprintf(stderr, "write error\n" );
+  //   if (bytes_read < 0)
+  //     fprintf(stderr, "read error\n" );
   }
 
   return 0;
