1dddbe3396d4e203f21815e1262a386cf5d3f17a
necisam <king4sam990816@gmail.com>
2016-10-30 15:44:20 +0800

    use chdir before lstat

--- assignment3/ftw8/ftw8.c
+++ assignment3/ftw8/ftw8.c
@@ -79,8 +79,7 @@ dopath(Myfunc* func)
 	struct stat		statbuf;
 	struct dirent	*dirp;
 	DIR				*dp;
-	int				ret, n;
-
+	int				ret;
 	if (lstat(fullpath, &statbuf) < 0)	/* stat error */
 		return(func(fullpath, &statbuf, FTW_NS));
 	if (S_ISDIR(statbuf.st_mode) == 0)	/* not a directory */
@@ -93,28 +92,21 @@ dopath(Myfunc* func)
 	if ((ret = func(fullpath, &statbuf, FTW_D)) != 0)
 		return(ret);
 
-	n = strlen(fullpath);
-	if (n + NAME_MAX + 2 > pathlen) {	/* expand path buffer */
-		pathlen *= 2;
-		if ((fullpath = realloc(fullpath, pathlen)) == NULL)
-			err_sys("realloc failed");
-	}
-	fullpath[n++] = '/';
-	fullpath[n] = 0;
 
 	if ((dp = opendir(fullpath)) == NULL)	/* can't read directory */
 		return(func(fullpath, &statbuf, FTW_DNR));
 
+	chdir(fullpath);
 	while ((dirp = readdir(dp)) != NULL) {
 		if (strcmp(dirp->d_name, ".") == 0  ||
-		    strcmp(dirp->d_name, "..") == 0)
+		    strcmp(dirp->d_name, "..") == 0){
 				continue;		/* ignore dot and dot-dot */
-		strcpy(&fullpath[n], dirp->d_name);	/* append name after "/" */
+		}
+		strcpy(fullpath,dirp->d_name);
 		if ((ret = dopath(func)) != 0)		/* recursive */
 			break;	/* time to leave */
 	}
-	fullpath[n-1] = 0;	/* erase everything from slash onward */
-
+	chdir("..");
 	if (closedir(dp) < 0)
 		err_ret("can't close directory %s", fullpath);
 	return(ret);
