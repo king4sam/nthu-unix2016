920cdb1a0a47aa9413eec02bd851c98969b57969
necisam <king4sam990816@gmail.com>
2016-11-18 15:06:43 +0800

    demo version

--- assignment4/pwd_cracker.c
+++ assignment4/pwd_cracker.c
@@ -4,22 +4,32 @@
 #define _XOPEN_SOURCE
 #define _GNU_SOURCE
 #include <unistd.h>
-#include <crypt.h>
+// #include <crypt.h>
+#include <omp.h>
 
 #define MAX_WORD 20
 #define NUM_WORDS 34
+#define MAX_LINE 2000
+#define DICTIONATY_FILE "john.txt"
 
-const char* to_guess = "$6$naIJPKfO$SMkeSkFM36M6u3mZIyf2hAtt31WxuYtoTwLMjF9Fv49cprYPKtR1K88Ox5xvQdLdoBrAOmCnomRvaHc7VDiqQ0";
+// const char* to_guess = "$6$naIJPKfO$SMkeSkFM36M6u3mZIyf2hAtt31WxuYtoTwLMjF9Fv49cprYPKtR1K88Ox5xvQdLdoBrAOmCnomRvaHc7VDiqQ0";
 
 
 int main(int argc, char **argv){
 
   if(argc < 2){
-    fprintf(stderr, "usage: ./decrypt <DICTIONARY FILE NAME>\n");
+    fprintf(stderr, "usage: ./decrypt <INPUT FILE NAME>\n");
   }
 
+  FILE* input = fopen(argv[1],"r");
+  char query[MAX_LINE];
+  fgets(query,MAX_LINE,input);
+  char* to_guess;
+  to_guess = strtok(query, ":");
+  to_guess = strtok(NULL, ":");
 
-  FILE* dic_f = fopen(argv[1],"r");
+
+  FILE* dic_f = fopen(DICTIONATY_FILE,"r");
   char* tar = strdup(to_guess);
   char *tmpstr = NULL;
   char* algo_no;
@@ -51,16 +61,19 @@ int main(int argc, char **argv){
   char * cs;
   int lengs[3];
   // linecount = 10;
+  #pragma omp parallel for
   for (int i = 0; i < linecount; i++){
-    printf("i %d\n", i);
+    // printf("i %d\n", i);
     guessword[0] = '\0';
     strcpy(guessword,words[i]);
     lengs[0] = word_lengs[i];
+    #pragma omp parallel for
     for (int j = 0; j < linecount; j++){
       // printf("j %d\n", j);
       guessword[lengs[0]] = '\0';
       strcat(guessword,words[j]);
       lengs[1] = word_lengs[j];
+      #pragma omp parallel for
       for (int k = 0; k < linecount; k++){
         guessword[lengs[0]+lengs[1]] = '\0';
         strcat(guessword,words[k]);
@@ -71,14 +84,9 @@ int main(int argc, char **argv){
         strtok(NULL, "$");
         cs = strtok(NULL, "$");
         if(strcmp(cs,cryptstr) == 0){
-          printf("hit , pwd is %s\n", guessword);
+          printf("%s\n", guessword);
           return 0;
         }
-
-        // crypt(guessword,salt_str);
-
-        // printf("after crypt : %s \n",cs);
-        // printf("guess %s\n", guessword);
       }
     }
   }
