d99c557c1cd22614b542104eb76907fd9d13f87a
necisam <king4sam990816@gmail.com>
2016-12-12 12:30:56 +0800

    foreground process grap control of stdin

--- assignment7/tsh.c
+++ assignment7/tsh.c
@@ -10,12 +10,16 @@
 #define MAXLINE 1000
 #define MAXARGV 10
 #define MAXCMD 100
+#define MAXJOBS 128
 const char prompt[] = "tsh >>> ";
 extern char **environ;
 
+pid_t control_process;
+
+
 void sig_fork(int signo)
 {
-
+    printf("childsig\n");
     pid_t pid;
     int stat;
     pid = waitpid(-1,&stat,0);
@@ -43,6 +47,12 @@ int do_buildind_cmd(char** doargv){
     rptr = getcwd(pathbuf,MAXPATHLENG);
     rptr == NULL? printf("%s\n",strerror(errno)) : printf("%s\n",pathbuf);
     return 1;
+  }
+  else if(strcmp(doargv[0], "jobs") == 0){
+
+  }
+  else if(strcmp(doargv[0], "fg") == 0){
+
   }
   else{
     return 0;
@@ -95,13 +105,32 @@ int main(int argc,char** argv){
   char cmd[MAXCMD];
   char line[MAXLINE];
   pid_t childpid;
+  pid_t parentpid;
+  int tmp;
 
+  // pid_t pgrpid = getpgid(getpid());
+  // pid_t fggrpid = tcgetpgrp(0);
+  // printf("mygrpid %d fdpgrpid %d \n", pgrpid, fggrpid);
+  //
+  control_process = getpid();
+  tcsetpgrp (STDIN_FILENO, control_process);
 
 
   while(1){
     printf("%s", prompt);
 
+
     if(fgets(line, MAXLINE, stdin) != NULL){
+      //control proess ignore sig after user input
+      signal (SIGINT, SIG_IGN);
+      signal (SIGQUIT, SIG_IGN);
+      signal (SIGTSTP, SIG_IGN);
+      signal (SIGHUP, SIG_IGN);
+      signal (SIGTTIN, SIG_IGN);
+      signal (SIGTTOU, SIG_IGN);
+      // signal(SIGINT,SIG_IGN);
+      // signal(SIGTSTP,ctrlz);
+
       line[strlen(line)-1] = '\0';
 
       //parse cmd to build doargc doarvg
@@ -123,25 +152,54 @@ int main(int argc,char** argv){
       //if not build-in cmd, execve
       if((childpid = fork()) == 0){
           // printf("child process\n");
+          childpid = getpid();
           setpgid(childpid,childpid);
+          //defalt sig handler
+          signal (SIGINT, SIG_DFL);
+          signal (SIGQUIT, SIG_DFL);
+          signal (SIGTSTP, SIG_DFL);
+          signal (SIGHUP, SIG_DFL);
+          signal (SIGTTIN, SIG_DFL);
+          signal (SIGTTOU, SIG_DFL);
+          printf("child pid %d pgid %d\n",childpid,getpgid(childpid));
           if(execvp(doargv[0], doargv) < 0){
             fprintf(stderr,"%s\n",strerror(errno));
             exit(0);
           }
-          // printf("child terminate\n");
+          printf("child terminate\n");
       }
       else if(childpid < 0){
         fprintf(stderr,"fork error\n");
       }
       //parent
       else{
+        parentpid = getpid();
+        printf("parent pid %d pgid %d\n",parentpid,getpgid(parentpid));
+        // printf("parent see child pid %d pgid %d\n",childpid,getpgid(childpid));
         if(bg){
-          // waitpid(-1, NULL, WNOHANG);
           signal(SIGCHLD, sig_fork);
         }
         if(!bg){
+          //set child process as foreground job
+          tmp = tcsetpgrp(STDIN_FILENO,childpid);
+          if(tmp == -1){
+            printf("tcsetpgrp error\n");
+          }
+
+          // set control process as foreground job
           waitpid(childpid, NULL, 0);
-          // printf("nobg wait\n");
+          tmp = tcsetpgrp(STDIN_FILENO,control_process);
+          if(tmp == -1){
+            printf("tcsetpgrp error\n");
+          }
+
+          //defalt sig handler
+          signal (SIGINT, SIG_DFL);
+          signal (SIGQUIT, SIG_DFL);
+          signal (SIGTSTP, SIG_DFL);
+          signal (SIGHUP, SIG_DFL);
+          signal (SIGTTIN, SIG_DFL);
+          signal (SIGTTOU, SIG_DFL);
         }
       }
 
